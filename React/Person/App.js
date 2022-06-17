bae9c40c9367f05a19718e599eb473d489e478f5
Sundar Paul <53091076+sundarpaul@users.noreply.github.com>
2022-06-17 13:13:00 +0530

    Create App.js

---
+++ React/Person/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Person from './components/Person/Person'
+
+function App() {
+    
+    return (
+        <>
+            <Person name='sundar' age='19' edu='diploma computer' phone='9999999999' city='HYD' />
+            <Person name='suraj' age='18' edu='12th' phone='9999999999' city='PUNE' />
+        </>
+    )
+}
+
+export default App;
